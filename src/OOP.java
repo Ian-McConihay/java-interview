bdd66c4cd08ce7650e6fe3237686584a833d3284
Ian McConihay <mcconihay.ian92@gmail.com>
2021-12-30 15:42:40 -0600

    First commit

---
+++ src/OOP.java
@@ -0,0 +1,14 @@
+public class OOP {
+// Four basic pillars Abstraction, Polymorphism, Inheritance, and Encapsulation.
+
+	public static int add(int num, int num2){
+		return num + num2;
+	}
+	void add();
+
+	public static void main(String[] args) {
+	System.out.println(add(1,2));
+
+	}
+}
+
