937ea9bca11d1a64d0d49b93ab3bc4804e05dac9
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-12 23:28:14 -0600

    Print the date class prints todays date

--- src/PrintDate.java
+++ src/PrintDate.java
@@ -1,2 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PrintDate {
+	public static void main(String[] args) {
+		String pattern = "MM-dd-yyyy";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+		String date = simpleDateFormat.format(new Date());
+		System.out.println(date);
+
+	}
 }
