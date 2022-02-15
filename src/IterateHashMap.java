37cccbcbe74098c81336bd76e9d8bd86d44abe3a
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-14 23:59:22 -0600

    Iterate hashmap

--- src/IterateHashMap.java
+++ src/IterateHashMap.java
@@ -1,2 +1,17 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class IterateHashMap {
+	public static void main(String[] arguments) {
+		// creating HashMap.
+		Map<Integer, String> hash_map = new HashMap<Integer, String>();
+		// inserting sets.
+		hash_map.put(1, "Thor");
+		hash_map.put(2, "Iron man");
+		// iterating using for loop.
+		for (Map.Entry<Integer, String> set : hash_map.entrySet()) {
+			System.out.println(set.getKey() + " = " + set.getValue());
+		}
+	}
 }
